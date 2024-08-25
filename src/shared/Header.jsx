22f323d866c017d71b47a5715fd29e864ae92636
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-08-25 14:56:51 +0600

    update nav

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -7,25 +7,25 @@ const Header = () => {
 
     const navBar = <>
 
-        <NavLink className="menu-btn" to="/">Home</NavLink>
-        <NavLink className="menu-btn" to="/sp-tools">SP Tool</NavLink>
-        <NavLink className="menu-btn" to="/spd-tools">SPD Tool</NavLink>
-        <NavLink className="menu-btn" to="/blogs">Blogs</NavLink>
-        <NavLink className="menu-btn" to="/">Solutions</NavLink>
+        <NavLink className={({ isActive }) => isActive ? 'active' : 'default'} to="/">Home</NavLink>
+        <NavLink className={({ isActive }) => isActive ? 'active' : 'default'} to="/sp-tools">SP Tool</NavLink>
+        <NavLink className={({ isActive }) => isActive ? 'active' : 'default'} to="/spd-tools">SPD Tool</NavLink>
+        <NavLink className={({ isActive }) => isActive ? 'active' : 'default'} to="/blogs">Blogs</NavLink>
+        <NavLink className={({ isActive }) => isActive ? 'active' : 'default'} to="/as">Solutions</NavLink>
     </>
     return (
         <header>
             <div className="my-container flex justify-between items-center ">
-                <Link to="/"><h2>Soft<span className='text-primary'>Firm</span>.com</h2></Link>
+                <Link to="/"><h2>SoftFirm.com</h2></Link>
 
                 <svg onClick={() => setOpen(!open)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 md:hidden cursor-pointer">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                 </svg>
                 <div className="md:block hidden">
-                {
-                    navBar
-                }
-            </div>
+                    {
+                        navBar
+                    }
+                </div>
             </div>
             <div className={`${open ? 'block md:hidden' : 'hidden'}`}>
                 {
