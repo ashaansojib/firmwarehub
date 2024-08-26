fd6a78dbc9c18ea17698f0ffbcce7245b687f1d0
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-08-26 11:01:00 +0600

    active dashboard nav link

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import DashboardAside from '../pages/dashboard/DashboardAside';
+import DashboardHeader from '../pages/dashboard/DashboardHeader';
 
 const Dashboard = () => {
     const router = useLocation();
     console.log(router.pathname);
     return (
-        <section>
-            <div className='flex gap-4'>
-                <DashboardAside />
-                <div>
-                    <header>Hello bara</header>
-                    {
-                        router.pathname === '/admin' ? <h2>Main Dashobad of home</h2> : <Outlet />
-                    }
-                </div>
+        <section className='flex justify-between'>
+            <DashboardAside />
+            <div className='w-full'>
+                <DashboardHeader />
+                {
+                    router.pathname === '/admin' ? <h2>Main Dashobad of home</h2> : <Outlet />
+                }
             </div>
         </section>
     );
--- src/pages/dashboard/DashboardAside.jsx
+++ src/pages/dashboard/DashboardAside.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 
 const DashboardAside = () => {
+    const loca = useLocation();
     return (
-        <aside className='w-[280px]'>
+        <aside className='w-[300px]'>
             <h3 className='border-b border-dotted p-4 text-center'>SoftFirm.com</h3>
             {/* menus */}
             <div className='text-secondary px-4'>
                 <span className='py-3 inline-block text-sm'>Menu</span>
                 <ul>
-                    <NavLink to="/" className='flex gap-2 px-3 py-2 hover:bg-[#9eabf5] hover:text-white rounded-md'>
+                    <NavLink to="/" className={`${loca.pathname === '/admin' ? 'd-active' : 'dashboard-menu'}`}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                         </svg>
@@ -18,19 +19,19 @@ const DashboardAside = () => {
                 </ul>
                 <span className='py-3 inline-block text-sm'>Services</span>
                 <ul>
-                    <NavLink to="/" className='flex gap-2 px-3 py-2 hover:bg-[#9eabf5] hover:text-white rounded-md'>
+                    <NavLink to="/" className='dashboard-menu'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
                         </svg>
                         <span>Add Product</span>
                     </NavLink>
-                    <NavLink to="/" className='flex gap-2 px-3 py-2 hover:bg-[#9eabf5] hover:text-white rounded-md'>
+                    <NavLink to="/" className='dashboard-menu'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                         </svg>
                         <span>Manage Product</span>
                     </NavLink>
-                    <NavLink to="/" className='flex gap-2 px-3 py-2 hover:bg-[#9eabf5] hover:text-white rounded-md'>
+                    <NavLink to="/" className='dashboard-menu'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 7.5.415-.207a.75.75 0 0 1 1.085.67V10.5m0 0h6m-6 0h-1.5m1.5 0v5.438c0 .354.161.697.473.865a3.751 3.751 0 0 0 5.452-2.553c.083-.409-.263-.75-.68-.75h-.745M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                         </svg>
