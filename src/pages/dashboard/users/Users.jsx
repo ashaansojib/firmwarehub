74385581a6f4c52de994d1228d1a907c25a1c6e1
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-10-10 00:34:23 +0600

    add user to the fronend

--- src/pages/dashboard/users/Users.jsx
+++ src/pages/dashboard/users/Users.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import { DTitile } from '../../../components/DTitle';
+import React from "react";
+import { DTitile } from "../../../components/DTitle";
+import { useGetUsersQuery } from "../../../redux/features/UserSlice";
+import MyLoader from "../../../components/MyLoader";
 
 const Users = () => {
-    return (
-        <div className='p-2'>
-            <DTitile title="Manage All Registerd Users!" />
-            
-        </div>
-    );
+  const { data: users, isLoading } = useGetUsersQuery();
+  return (
+    <div className="p-2">
+      <DTitile title="Manage All Registerd Users!" />
+      <div className="md:flex gap-2 my-2">
+        {isLoading ? (
+          <MyLoader />
+        ) : (
+          users?.data.map((user) => (
+            <div key={user._id} className="p-2 border mb-2 relative rounded-md">
+              <h3>{user.name}</h3>
+              <p>{user.email}</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="size-5 absolute top-0 text-red-500 right-0 cursor-pointer"
+              >
+                <path d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Users;
